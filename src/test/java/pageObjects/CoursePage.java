7eecc9f8821747029e4f5951bd05606b9e43249c
R <2461881@cognizant.com>
2026-02-12 15:29:42 +0530

    added codes

--- src/test/java/pageObjects/CoursePage.java
+++ src/test/java/pageObjects/CoursePage.java
@@ -1,5 +1,22 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CoursePage {
     // code for course page should be added here
+    WebDriver driver;
+
+
+    public CoursePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Web Elements
+    
+
+
 }
